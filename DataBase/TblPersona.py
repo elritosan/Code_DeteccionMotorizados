040ee813b06bb8a55c5539c25c54fdbd0d6ed4cc
Hugo Vargas <hugo.vargas1772@gmail.com>
2025-01-22 16:44:13 -0500

    Agregando Personas a 'TblPersona'

--- DataBase/TblPersona.py
+++ DataBase/TblPersona.py
@@ -2,9 +2,11 @@
 
 # Psudo-Base de Datos de personas
 usuarios_db = {
-    "ID811E": {"nombre": "Hugo Vargas", "telefono": "+593996907332", "correo": "dotafrozen1@gmail.com"},
-    "XYZ456": {"nombre": "María López", "telefono": "+593998888888", "correo": "maria.lopez@email.com"},
-    # Agregar más usuarios según sea necesario
+    "ID811E": {"nombre": "Hugo Vargas", "telefono": "+593996907332", "correo": "vangamarca4@gmail.com"},
+    "AA5601": {"nombre": "Dayana Paladines", "telefono": "+593983045751", "correo": "dayana.paladines@espoch.edu.ec"},
+    "HA0501": {"nombre": "Alex Vallejo", "telefono": "+593980551466", "correo": "alexvallejo325@gmail.com"},
+    "AP5001": {"nombre": "Marco Angamarca", "telefono": "+5930999859689", "correo": "vangamarca4@gmail.com"},
+    "GU0201": {"nombre": "Daniela Robalino", "telefono": "+593995676927", "correo": "catherin.robalino@espoch.edu.ec"},
 }
 
 # Función para obtener información de la persona por placa
