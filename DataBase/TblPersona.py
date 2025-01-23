9b3fc90c3f8ed86939d7eaef195a87085b2fd14d
Hugo Vargas <hugo.vargas1772@gmail.com>
2025-01-23 10:52:49 -0500

    Modificado 'TblPersona'

--- DataBase/TblPersona.py
+++ DataBase/TblPersona.py
@@ -2,7 +2,7 @@
 
 # Psudo-Base de Datos de personas
 usuarios_db = {
-    "ID811E": {"nombre": "Hugo Vargas", "telefono": "+593996907332", "correo": "vangamarca4@gmail.com"},
+    "ID811E": {"nombre": "Hugo Vargas", "telefono": "+593996907332", "correo": "dotafrozen1@gmail.com"},
     "AA5601": {"nombre": "Dayana Paladines", "telefono": "+593983045751", "correo": "dayana.paladines@espoch.edu.ec"},
     "HA0501": {"nombre": "Alex Vallejo", "telefono": "+593980551466", "correo": "alexvallejo325@gmail.com"},
     "AP5001": {"nombre": "Marco Angamarca", "telefono": "+5930999859689", "correo": "vangamarca4@gmail.com"},
